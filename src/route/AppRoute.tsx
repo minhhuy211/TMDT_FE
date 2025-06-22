9f981d14858246c16e6e349d5b8cc51573b4db9a
nguyenvanminhhuy <huycattuong0202@gmail.com>
2025-06-22 18:32:36 +0700

    update

--- src/route/AppRoute.tsx
+++ src/route/AppRoute.tsx
@@ -1,25 +1,25 @@
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HomePage } from "../pages/Home/HomePage";
 import { Service } from "@/pages/Service/Service.tsx";
 import { Contact } from "../pages/Contact/Contact";
 import MasterLayout from "../layout/MasterLayout/MasterLayout";
 import ProductPage from "../pages/Products/Product";
+import ProductPageAdmin from "../components/admin-data/ProductPage";
 import { Cart } from "../pages/Cart/Cart";
 import { Login } from "../pages/Login/Login";
-import AddressPage from '@/account/AddressPage'; // hoặc đúng theo đường dẫn của bạn
-import OrderHistory from "@/account/OrderHistory.tsx"
-import ProductDetail from "../pages/Products/Product-detail";  // Trang chi tiết sản phẩm
-import CheckoutPage from '../pages/Products/CheckoutPage';
+import AddressPage from "@/account/AddressPage"; // hoặc đúng theo đường dẫn của bạn
+import OrderHistory from "@/account/OrderHistory.tsx";
+import ProductDetail from "../pages/Products/Product-detail"; // Trang chi tiết sản phẩm
+import CheckoutPage from "../pages/Products/CheckoutPage";
 import PaymentSuccessPage from "../pages/Products/PaymentSuccessPage"; // Import success page
 // import NotificationsPage from "../account/NotificationsPage";
 
 import Dashboard from "../components/admin/Dashboard.tsx";
 import PageAdmin from "@/components/admin/SideBarData";
-import {ForgotPassword} from "@/pages/ForgetPassword/ForgotPassword.tsx";
-import AccountManagement from '@/account/ProfilePage.tsx';
-import ChangePassword from '@/account/ChangePassword';
-import UserCart from '@/account/user-cart.tsx';
+import { ForgotPassword } from "@/pages/ForgetPassword/ForgotPassword.tsx";
+import AccountManagement from "@/account/ProfilePage.tsx";
+import ChangePassword from "@/account/ChangePassword";
+import UserCart from "@/account/user-cart.tsx";
 
 import OAuth2RedirectHandler from "@/pages/Login/OAuth2RedirectHandler";
 import ProfilePage from "@/account/ProfilePage.tsx";
@@ -33,8 +33,6 @@ import CustomerPage from "../components/admin-data/CustomerPage";
 import OrderPage from "@/components/admin-data/OrderPage";
 import CategoryPage from "@/components/admin-data/CategoryPage";
 
-
-
 const route = createBrowserRouter([
   {
     path: "/",
@@ -44,14 +42,13 @@ const route = createBrowserRouter([
 
       { path: "/address", element: <AddressPage /> },
       // {path :"/notifications", element: <NotificationsPage />},
-      { path : "/order-history", element: <OrderHistory />},
-      {path: "/forgot-password", element: <ForgotPassword/>},
+      { path: "/order-history", element: <OrderHistory /> },
+      { path: "/forgot-password", element: <ForgotPassword /> },
       { path: "/", element: <HomePage /> },
-
 
       { path: "/product", element: <ProductPage /> },
       { path: "/productCustom", element: <UploadDesignsPage /> },
-       { path: "/about", element: <Service /> },
+      { path: "/about", element: <Service /> },
       { path: "/contact", element: <Contact /> },
       { path: "/cart", element: <Cart /> },
       { path: "/login", element: <Login /> },
@@ -68,21 +65,19 @@ const route = createBrowserRouter([
 
       { path: "/register", element: <Register /> },
       { path: "/profile", element: <ProfilePage /> },
-
-
     ],
   },
   {
     path: "/admin",
     element: <PageAdmin />,
     children: [
-      {path: "/admin", element: <Dashboard />},
+      { path: "/admin", element: <Dashboard /> },
       { path: "/admin/customer", element: <CustomerPage /> }, // 👈 đã thêm route customer
       { path: "/admin/order", element: <OrderPage /> }, // 👈 THÊM DÒNG NÀY
-      {  path: "/admin/category",
-        element: <CategoryPage />}
+      { path: "/admin/category", element: <CategoryPage /> },
+      { path: "/admin/product", element: <ProductPageAdmin /> },
     ],
-  }
+  },
 ]);
 
 const AppRoute = () => <RouterProvider router={route} />;
